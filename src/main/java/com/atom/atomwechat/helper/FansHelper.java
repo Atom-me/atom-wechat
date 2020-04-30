be367b91646d4246ffdfcadc4ded9d37c2ce7910
liuyaming <sarming@126.com>
2020-04-30 22:45:24 +0800

    use static import

--- src/main/java/com/atom/atomwechat/helper/FansHelper.java
+++ src/main/java/com/atom/atomwechat/helper/FansHelper.java
@@ -1,13 +1,15 @@
 package com.atom.atomwechat.helper;
 
-import com.atom.atomwechat.constant.WeChatConstant;
-import com.atom.atomwechat.model.fans.Subscriber;
 import com.atom.atomwechat.model.fans.FansList;
+import com.atom.atomwechat.model.fans.Subscriber;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+
+import static com.atom.atomwechat.constant.WeChatConstant.FANS_INFO_URL;
+import static com.atom.atomwechat.constant.WeChatConstant.FANS_LIST_URL;
 
 /**
  * @author atom
@@ -23,13 +25,13 @@ public class FansHelper {
     private AccessTokenHelper accessTokenHelper;
 
     public FansList list() {
-        FansList fansLis = restTemplate.getForObject(WeChatConstant.FANS_LIST_URL, FansList.class, accessTokenHelper.fetchAccessToken().getAccessToken(), "");
+        FansList fansLis = restTemplate.getForObject(FANS_LIST_URL, FansList.class, accessTokenHelper.fetchAccessToken().getAccessToken(), "");
         log.info(fansLis.toString());
         return fansLis;
     }
 
     public Subscriber info(String openId) {
-        Subscriber subscriber = restTemplate.getForObject(WeChatConstant.FANS_INFO_URL, Subscriber.class, accessTokenHelper.fetchAccessToken().getAccessToken(), openId);
+        Subscriber subscriber = restTemplate.getForObject(FANS_INFO_URL, Subscriber.class, accessTokenHelper.fetchAccessToken().getAccessToken(), openId);
         log.info(subscriber.toString());
         return subscriber;
     }
