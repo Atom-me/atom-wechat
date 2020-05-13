af12f6ac076ca69c6040b6451e6d7c897dc4a30e
liuyaming <sarming@126.com>
2020-05-13 14:59:09 +0800

    完善 注释

--- src/main/java/com/atom/atomwechat/handler/event/ReqTemplateSendJobFinishEventMsgHandler.java
+++ src/main/java/com/atom/atomwechat/handler/event/ReqTemplateSendJobFinishEventMsgHandler.java
@@ -25,6 +25,73 @@ public class ReqTemplateSendJobFinishEventMsgHandler implements ReqMsgHandler<Re
         log.info("==========={}=============", EVENT_TEMPLATE_SEND_JOB_FINISH.getDesc());
 
         //todo process event response
+        /**
+         * 在模版消息发送任务完成后，微信服务器会将是否送达成功作为通知，发送到开发者在开发模式中填写的URL中
+         * 在模版消息发送任务完成后，微信服务器会将是否送达成功作为通知，发送到开发者在开发模式中填写的URL中。
+         *
+         * 1、送达成功时，推送的XML如下：
+         *
+         * 	<xml>
+         *
+         * 	<ToUserName><![CDATA[gh_7f083739789a]]></ToUserName>
+         *
+         * 	<FromUserName><![CDATA[oia2TjuEGTNoeX76QEjQNrcURxG8]]></FromUserName>
+         *
+         * 	<CreateTime>1395658920</CreateTime>
+         *
+         * 	<MsgType><![CDATA[event]]></MsgType>
+         *
+         * 	<Event><![CDATA[TEMPLATESENDJOBFINISH]]></Event>
+         *
+         * 	<MsgID>200163836</MsgID>
+         *
+         * 	<Status><![CDATA[success]]></Status>
+         *
+         * 	</xml>
+         *
+         *
+         * 2、送达由于用户拒收（用户设置拒绝接收公众号消息）而失败时，推送的XML如下：
+         *
+         * 	<xml>
+         *
+         * 	<ToUserName><![CDATA[gh_7f083739789a]]></ToUserName>
+         *
+         * 	<FromUserName><![CDATA[oia2TjuEGTNoeX76QEjQNrcURxG8]]></FromUserName>
+         *
+         * 	<CreateTime>1395658984</CreateTime>
+         *
+         * 	<MsgType><![CDATA[event]]></MsgType>
+         *
+         * 	<Event><![CDATA[TEMPLATESENDJOBFINISH]]></Event>
+         *
+         * 	<MsgID>200163840</MsgID>
+         *
+         * 	<Status><![CDATA[failed:user block]]></Status>
+         *
+         * 	</xml>
+         *
+         *
+         * 3、送达由于其他原因失败时，推送的XML如下：
+         *
+         * 	<xml>
+         *
+         * 	<ToUserName><![CDATA[gh_7f083739789a]]></ToUserName>
+         *
+         * 	<FromUserName><![CDATA[oia2TjuEGTNoeX76QEjQNrcURxG8]]></FromUserName>
+         *
+         * 	<CreateTime>1395658984</CreateTime>
+         *
+         * 	<MsgType><![CDATA[event]]></MsgType>
+         *
+         * 	<Event><![CDATA[TEMPLATESENDJOBFINISH]]></Event>
+         *
+         * 	<MsgID>200163840</MsgID>
+         *
+         * 	<Status><![CDATA[failed: system failed]]></Status>
+         *
+         * 	</xml>
+         *
+         */
         log.info(reqTemplateSendJobFinishEventMessage.toString());
 
 
